30cb12b8feda2034d54090405a046d19f1a175c6
Tyler <Tyler@heshmati.com>
2021-04-02 20:20:18 -0700

    added error message

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,5 +1,8 @@
 module.exports = (string) => {
     input = string + ''
-    if (typeof (string) === 'undefined') return "DJS-UTILS: ERROR STRING IS UNDFINED"
+    if (typeof (string) === 'undefined') {
+        console.error("[DJS-UTILS][ERROR] STRING IS UNDFINED")
+        return undefined
+    }
     else { return input.split(' ').map(str => str.slice(0, 1).toUpperCase() + str.slice(1)).join(' '); }
 };
