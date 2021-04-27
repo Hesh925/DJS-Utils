241d7ffb0c032c411a9453134ff0c6a1cb2d2471
tyhesh <69137806+tyhesh@users.noreply.github.com>
2021-04-27 13:18:58 -0700

    Update guildLog.js

--- src/utils/guildLog.js
+++ src/utils/guildLog.js
@@ -2,5 +2,5 @@ const fs = require('fs')
 const utils = require('../main.js')
 module.exports = (message = String) => {
     var content = `${utils.getDate()} | ${message}\n`
-    fs.writeFile(`./guild.log`, content, { flag: 'a+' }, err => {})
+    fs.writeFile(`./log/guild.log`, content, { flag: 'a+' }, err => {})
 }
