754bb69fb4c73917dd0aa8f77a8e4cf32a57103e
Tyler <41883777+Havoc925@users.noreply.github.com>
2021-09-20 01:47:37 -0700

    Add files via upload

---
+++ src/utils/setRichPresence.js
@@ -0,0 +1,9 @@
+const config = require("../config/CONFIG.json");
+module.exports = (client) => {
+	const types = [ "PLAYING", "STREAMING", "LISTENING", "WATCHING", "COMPETING" ];
+	const env = process.argv.slice(2)[0] === "-dev" ? "dev" : "prod";
+	
+	client.user.setActivity((config.envSettings[env].status.status), { type: ( types[config.envSettings[env].status.type] ) } );
+
+	//client.user.setActivity((message ? message : "the waiting game"), { type: ( activity ? types[activity] : "PLAYING" ) } );
+};
