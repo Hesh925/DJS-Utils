b84cb7256d26ed4db79415c48542dbaa5f45840f
Tyler <41883777+Havoc925@users.noreply.github.com>
2021-10-16 00:48:11 -0700

    Update setRichPresence.js

--- src/utils/setRichPresence.js
+++ src/utils/setRichPresence.js
@@ -1,6 +1,6 @@
 module.exports = (client, config) => {
 	const types = [ "PLAYING", "STREAMING", "LISTENING", "WATCHING", "COMPETING" ];
-	const env = process.argv.slice(2)[0] === "-dev" ? "dev" : "prod";
+	const env = utils.searchArgv("env", true) === "dev" ? "dev" : "prod";
 	
 	client.user.setActivity((config.envSettings[env].status.status), { type: ( types[config.envSettings[env].status.type] ) } );
 
