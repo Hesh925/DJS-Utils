c670af19a59c739345819ad9252485aa3493c2a5
Tyler <Tyler@heshmati.com>
2021-10-15 23:21:46 -0700

    eslint issues

--- src/utils/ytSearch.js
+++ src/utils/ytSearch.js
@@ -1,15 +1,13 @@
-/* eslint-disable multiline-comment-style, capitalized-comments */
-const utils = require("../main");
+/* eslint-disable multiline-comment-style, capitalized-comments, no-undefined */
 const ytsr = require("ytsr");
-const ytdl = require("ytdl-core");
 
 module.exports = async (message) => {
 	const searchTerm = (String(message).replace(/,/g, " "));
 	if(message !== null || message !== undefined) {
-		//if (ytdl.validateURL(searchTerm) || ytdl.validateID(searchTerm)){
-			const videoData = await ytsr(searchTerm, {limit: 1, pages : 1});
-			return videoData["items"][0];
-		//} else return null;
+		// if (ytdl.validateURL(searchTerm) || ytdl.validateID(searchTerm)){
+		const videoData = await ytsr(searchTerm, {limit: 1, pages : 1});
+		return videoData["items"][0];
+		// } else return null;
 	} else return null;
 };
 
