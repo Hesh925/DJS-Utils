5d5e359cdf4cd622ef00118767c876dcfed6e5ea
Tyler <41883777+Havoc925@users.noreply.github.com>
2021-10-15 23:32:24 -0700

    Update searchArgv.js

--- src/utils/searchArgv.js
+++ src/utils/searchArgv.js
@@ -1,13 +1,12 @@
 const argv = process.argv.slice(2);
 
 module.exports = async (message) => {
-	const st = `-${ message }`;
+	const st = message.startsWith("-") ? message : `-${ message }`;
 	if (argv.includes(st)) {
 		var index = argv.findIndex(function (argv) {
 			return argv.includes(st);
 		});
 		const res = argv[index + 1]; // .slice(lookingFor.length);
-		console.log(res);
 		return res;
 	}
 };
