d9d2e87d2721b6b7fff7483a861355ad7b054ea3
Tyler <41883777+Havoc925@users.noreply.github.com>
2021-10-16 23:41:08 -0700

    Delete isBotOwner.js

--- src/utils/isBotOwner.js
+++
@@ -1,7 +0,0 @@
-const config = require("../config.json");
-
-module.exports = (message, ownerID) => {
-	const owner = ownerID || config.Owner;
-	if (message.author.id === owner) return true;
-	else return false;
-};
