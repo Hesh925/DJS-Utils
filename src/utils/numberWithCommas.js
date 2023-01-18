d0ab7983a5a48575e5bfa4000d22aae4908f2a71
Tyler <Tyler@heshmati.com>
2023-01-18 01:32:51 -0800

    fixed new utils

--- src/utils/numberWithCommas.js
+++ src/utils/numberWithCommas.js
@@ -1,8 +1,6 @@
-module.exports = {
-	run(x) {
-		var parts = x.toString().split(".");
-		// eslint-disable-next-line prefer-named-capture-group
-		parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		return parts.join(".");
-	}
+module.exports = (x) => {
+	var parts = x.toString().split(".");
+	// eslint-disable-next-line prefer-named-capture-group
+	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	return parts.join(".");
 };
